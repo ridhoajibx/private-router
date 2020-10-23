9cf12dddd1003f408a170763610967e2f0c9918d
ridhoajibx <ridhoajibx@gmail.com>
2020-10-23 08:15:23 +0700

    [ridho] refactor header

--- src/layouts/app/DashboardLayouts.js
+++ src/layouts/app/DashboardLayouts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import Header from './components/Header';
+import Header from '../../components/Header';
 
 const DashboardLayouts = (props) => {
     return (

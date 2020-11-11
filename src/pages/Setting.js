108f6c30227af01350439e9b78d3bdac4626b43d
ridhoajibx <ridhoajibx@gmail.com>
2020-11-11 22:40:16 +0700

    [ridho] refactor settings password

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button, Card, CardBody, Row, Col } from 'reactstrap';
 const Setting = (props) => {
     const [state, setState] = useState({
         password: '',
@@ -15,32 +15,39 @@ const Setting = (props) => {
     const onSubmit = (e) => {
         // this is code 
         e.preventDefault()
-        console.log(setState({
+        setState({
             password: e.target.value,
             confirmPass: e.target.value,
-        }));
+        });
     }
-    console.log(props, "cek props");
+    console.log(state, 'cek state');
     return (
         <div className={`content-wrapper content-wrapper--${!props.toggleSide ? 'show' : 'hide'}`}>
             <span className="toggle-btn" onClick={props.handleToggleSide}>
                 {!props.toggleSide ? <FaTimes /> : <FaBars />}
-                <h4>Setting Password</h4>
+                <h4>Settings</h4>
             </span>
-            <br></br>
-            <br></br>
-            <br></br>
-            <Form onSubmit={onSubmit}>
-                <FormGroup>
-                    <Label for="Password">New Password</Label>
-                    <Input type="text" name="password" id="Password" placeholder="Password" value={state.password} onChange={onHandler} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="confirmPassword">Confirm Password</Label>
-                    <Input type="confirmpassword" name="confirmpass" value={state.confirmpass} onChange={onHandler} id="confirmPassword" placeholder="confirm password" />
-                </FormGroup>
-                <Button>submit</Button>
-            </Form>
+            <Row className="mt-5">
+                <Col sm="12">
+                    <Card>
+                        <CardBody>
+                            <Form onSubmit={onSubmit}>
+                                <FormGroup>
+                                    <Label for="Password">New Password</Label>
+                                    <Input type="password" name="password" id="Password" placeholder="Password" value={state.password} onChange={onHandler} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="confirmPassword">Confirm Password</Label>
+                                    <Input type="password" name="confirmpass" value={state.confirmpass} onChange={onHandler} id="confirmPassword" placeholder="confirm password" />
+                                </FormGroup>
+                                <div className="d-flex justify-content-center">
+                                    <Button size='lg' color='primary' style={{ fontSize: '16px' }}>submit</Button>
+                                </div>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
         </div>
 
     )

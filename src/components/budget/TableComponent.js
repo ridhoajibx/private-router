10df348730e39457f2f84552f299e6df1c579dd8
ridhoajibx <ridhoajibx@gmail.com>
2020-11-18 02:49:02 +0700

    CI/CD & create alert login & register

--- src/components/budget/TableComponent.js
+++ src/components/budget/TableComponent.js
@@ -1,146 +1,146 @@
-import React from "react";
-import BootstrapTable from "react-bootstrap-table-next";
-import { Container, Button, Row, Col, Spinner } from "reactstrap";
-import { FaBookmark, FaCalendar, FaCog, FaHome } from 'react-icons/fa';
-import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
-import paginationFactory from "react-bootstrap-table2-paginator";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import swal from 'sweetalert';
-// import { deleteUser } from "../actions/userAction";
+// import React from "react";
+// import BootstrapTable from "react-bootstrap-table-next";
+// import { Container, Button, Row, Col, Spinner } from "reactstrap";
+// import { FaBookmark, FaCalendar, FaCog, FaHome } from 'react-icons/fa';
+// import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+// import paginationFactory from "react-bootstrap-table2-paginator";
+// import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
+// import swal from 'sweetalert';
+// // import { deleteUser } from "../actions/userAction";
 
-const { SearchBar } = Search;
+// const { SearchBar } = Search;
 
-const handleClick = (dispatch, id) => {
+// const handleClick = (dispatch, id) => {
   
-  swal({
-    title: "Apakah Anda yakin akan menghapus data ini ?",
-    icon: "warning",
-    buttons: true,
-    dangerMode: true,
-  })
-  .then((willDelete) => {
-    if (willDelete) {
-      dispatch()
-      swal("Data User Sukses dihapus", {
-        icon: "success",
-      });
-    } else {
-      swal("Data gagal dihapus");
-    }
-  });
-}
+//   swal({
+//     title: "Apakah Anda yakin akan menghapus data ini ?",
+//     icon: "warning",
+//     buttons: true,
+//     dangerMode: true,
+//   })
+//   .then((willDelete) => {
+//     if (willDelete) {
+//       dispatch()
+//       swal("Data User Sukses dihapus", {
+//         icon: "success",
+//       });
+//     } else {
+//       swal("Data gagal dihapus");
+//     }
+//   });
+// }
 
 
-const defaultSorted = [
-  {
-    dataField: "id",
-    order: "asc",
-  },
-];
+// const defaultSorted = [
+//   {
+//     dataField: "id",
+//     order: "asc",
+//   },
+// ];
 
-const mapStateToProps = (state) => {
-  return {
-    getUsersList: state.users.getUsersList,
-    errorUsersList: state.users.errorUsersList,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     getUsersList: state.users.getUsersList,
+//     errorUsersList: state.users.errorUsersList,
+//   };
+// };
 
-const TableComponent = (props) => {
+// const TableComponent = (props) => {
 
-  const columns = [
-    {
-      dataField: "id",
-      text: "ID",
-      sort: true,
-      headerStyle: () => {
-        return { width: "5%" };
-      },
-    },
-    {
-      dataField: "nama",
-      text: "Nama",
-      sort: true,
-    },
-    {
-      dataField: "alamat",
-      text: "Alamat",
-      sort: true,
-    },
-    {
-      dataField: "link",
-      text: "Action",
-      formatter: (rowContent, row) => {
-        return (
-          <div>
-            <Link to={"detail/" + row.id}>
-              <Button color="dark" className="mr-2">
-              <FaCalendar /> Detail
-              </Button>
-            </Link>
+//   const columns = [
+//     {
+//       dataField: "id",
+//       text: "ID",
+//       sort: true,
+//       headerStyle: () => {
+//         return { width: "5%" };
+//       },
+//     },
+//     {
+//       dataField: "nama",
+//       text: "Nama",
+//       sort: true,
+//     },
+//     {
+//       dataField: "alamat",
+//       text: "Alamat",
+//       sort: true,
+//     },
+//     {
+//       dataField: "link",
+//       text: "Action",
+//       formatter: (rowContent, row) => {
+//         return (
+//           <div>
+//             <Link to={"detail/" + row.id}>
+//               <Button color="dark" className="mr-2">
+//               <FaCalendar /> Detail
+//               </Button>
+//             </Link>
   
-            <Link to={"edit/" + row.id}>
-              <Button color="dark" className="mr-2">
-              <FaCog /> Edit
-              </Button>
-            </Link>
+//             <Link to={"edit/" + row.id}>
+//               <Button color="dark" className="mr-2">
+//               <FaCog /> Edit
+//               </Button>
+//             </Link>
   
-            <Button color="dark" className="mr-2" onClick={() => handleClick(props.dispatch, row.id)}>
-            <FaBookmark /> Delete
-            </Button>
-          </div>
-        );
-      },
-    },
-  ];
+//             <Button color="dark" className="mr-2" onClick={() => handleClick(props.dispatch, row.id)}>
+//             <FaBookmark /> Delete
+//             </Button>
+//           </div>
+//         );
+//       },
+//     },
+//   ];
 
   
-  return (
-    <Container>
-      {props.getUsersList ? (
-        <ToolkitProvider
-          bootstrap4
-          keyField="id"
-          data={props.getUsersList}
-          columns={columns}
-          defaultSorted={defaultSorted}
-          search
-        >
-          {(props) => (
-            <div>
-              <Row>
-                <Col>
-                  <Link to="/create">
-                    <Button color="dark" className="mr-2">
-                    < FaHome /> Create User
-                    </Button>
-                  </Link>
-                </Col>
-                <Col>
-                  <div className="float-right">
-                    <SearchBar placeholder="Search .." />
-                  </div>
-                </Col>
-              </Row>
+//   return (
+//     <Container>
+//       {props.getUsersList ? (
+//         <ToolkitProvider
+//           bootstrap4
+//           keyField="id"
+//           data={props.getUsersList}
+//           columns={columns}
+//           defaultSorted={defaultSorted}
+//           search
+//         >
+//           {(props) => (
+//             <div>
+//               <Row>
+//                 <Col>
+//                   <Link to="/create">
+//                     <Button color="dark" className="mr-2">
+//                     < FaHome /> Create User
+//                     </Button>
+//                   </Link>
+//                 </Col>
+//                 <Col>
+//                   <div className="float-right">
+//                     <SearchBar placeholder="Search .." />
+//                   </div>
+//                 </Col>
+//               </Row>
 
-              <BootstrapTable
-                {...props.baseProps}
-                pagination={paginationFactory()}
-              />
-            </div>
-          )}
-        </ToolkitProvider>
-      ) : (
-        <div className="text-center">
-          {props.errorUsersList ? (
-            <h4>{props.errorUsersList}</h4>
-          ) : (
-            <Spinner color="dark" />
-          )}
-        </div>
-      )}
-    </Container>
-  );
-};
+//               <BootstrapTable
+//                 {...props.baseProps}
+//                 pagination={paginationFactory()}
+//               />
+//             </div>
+//           )}
+//         </ToolkitProvider>
+//       ) : (
+//         <div className="text-center">
+//           {props.errorUsersList ? (
+//             <h4>{props.errorUsersList}</h4>
+//           ) : (
+//             <Spinner color="dark" />
+//           )}
+//         </div>
+//       )}
+//     </Container>
+//   );
+// };
 
-export default connect(mapStateToProps, null)(TableComponent);
+// export default connect(mapStateToProps, null)(TableComponent);
